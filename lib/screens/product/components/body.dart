e6f12fbf5e8aadc84a1c5ab4f0c824d400f010bb
brianmujjuni <brianmujjuni@engineer.com>
2025-07-17 23:54:44 +0300

    add callback button

--- lib/screens/product/components/body.dart
+++ lib/screens/product/components/body.dart
@@ -10,40 +10,43 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SearchBox(
-          onChange: (value) {},
-        ),
-        Categorylist(),
-        SizedBox(
-          height: kDefaultPadding / 2,
-        ),
-        Expanded(
-          child: Stack(
-            children: [
-              //our background
-              Container(
-                margin: EdgeInsets.only(top: 70),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        children: [
+          SearchBox(
+            onChange: (value) {},
+          ),
+          Categorylist(),
+          SizedBox(
+            height: kDefaultPadding / 2,
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                //our background
+                Container(
+                  margin: EdgeInsets.only(top: 70),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
                   ),
                 ),
-              ),
-              ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (context, index) => ProductCard(
-                  itemIndex: index,
-                  product: products[index],
-                ),
-              )
-            ],
-          ),
-        )
-      ],
+                ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) => ProductCard(
+                    itemIndex: index,
+                    product: products[index],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/product/components/product_card.dart
+++ lib/screens/product/components/product_card.dart
@@ -4,10 +4,12 @@ import 'package:furniture_app/models/products.dart';
 class ProductCard extends StatelessWidget {
   final int itemIndex;
   final Product product;
+  final VoidCallback press;
   const ProductCard({
     super.key,
     required this.itemIndex,
-    required this.product
+    required this.product,
+    required this.press
   });
 
   @override
@@ -20,77 +22,80 @@ class ProductCard extends StatelessWidget {
       ),
       // color: Colors.blueAccent,
       height: 160,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          //THESE ARE THE BACKGROUND
-          Container(
-            height: 136,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(22),
-                color: kBlueColor,
-                boxShadow: [kDefaultShadow]),
-            child: Container(
-              margin: EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(22),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 160,
-              width: 200,
-              child: Image.asset(
-                "assets/images/Item_1.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: SizedBox(
+      child: InkWell(
+        onTap: press,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            //THESE ARE THE BACKGROUND
+            Container(
               height: 136,
-              width: size.width - 200,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    child: Text(
-                      "Classic Leather Arm Chair",
-                      style: Theme.of(context).textTheme.labelLarge,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: kDefaultPadding * 1.5, //30 padding
-                      vertical: kDefaultPadding / 4, //5 top and bottom
-                    ),
-                    decoration: BoxDecoration(
-                      color: kSecondaryColor,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(22),
-                        topRight: Radius.circular(22),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(22),
+                  color: itemIndex.isEven ? kBlueColor : kSecondaryColor,
+                  boxShadow: [kDefaultShadow]),
+              child: Container(
+                margin: EdgeInsets.only(right: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                height: 160,
+                width: 200,
+                child: Image.asset(
+                  product.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: SizedBox(
+                height: 136,
+                width: size.width - 200,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                      child: Text(
+                        product.title,
+                        style: Theme.of(context).textTheme.labelLarge,
                       ),
                     ),
-                    child: Text(
-                      "\$58",
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  )
-                ],
+                    Spacer(),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: kDefaultPadding * 1.5, //30 padding
+                        vertical: kDefaultPadding / 4, //5 top and bottom
+                      ),
+                      decoration: BoxDecoration(
+                        color: kSecondaryColor,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(22),
+                          topRight: Radius.circular(22),
+                        ),
+                      ),
+                      child: Text(
+                        "\$${product.price}",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
