c3c7962ade8e42e4fea6173074c373ada14ce0d5
brianmujjuni <brianmujjuni@engineer.com>
2025-07-17 14:09:53 +0300

    add text to category card

--- lib/screens/product/components/body.dart
+++ lib/screens/product/components/body.dart
@@ -47,6 +47,7 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Container(
       margin: EdgeInsets.symmetric(
         horizontal: kDefaultPadding,
@@ -82,6 +83,45 @@ class ProductCard extends StatelessWidget {
               child: Image.asset(
                 "assets/images/Item_1.png",
                 fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              height: 136,
+              width: size.width - 200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    child: Text(
+                      "Classic Leather Arm Chair",
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding * 1.5, //30 padding
+                      vertical: kDefaultPadding / 4, //5 top and bottom
+                    ),
+                    decoration: BoxDecoration(
+                      color: kSecondaryColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(22),
+                        topRight: Radius.circular(22),
+                      ),
+                    ),
+                    child: Text(
+                      "\$58",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  )
+                ],
               ),
             ),
           )
