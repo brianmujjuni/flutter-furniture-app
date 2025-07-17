f7b0e519701313c90d3348378f4eaf92d999326e
brianmujjuni <brianmujjuni@engineer.com>
2025-07-17 23:23:54 +0300

    pass product as prop in the product card

--- lib/screens/product/components/body.dart
+++ lib/screens/product/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:furniture_app/components/search_box.dart';
 import 'package:furniture_app/constants.dart';
+import 'package:furniture_app/models/products.dart';
 import 'package:furniture_app/screens/product/components/category_list.dart';
+import 'package:furniture_app/screens/product/components/product_card.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -32,104 +34,16 @@ class Body extends StatelessWidget {
                 ),
               ),
               ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) => ProductCard(),
+                itemCount: products.length,
+                itemBuilder: (context, index) => ProductCard(
+                  itemIndex: index,
+                  product: products[index],
+                ),
               )
             ],
           ),
         )
       ],
-    );
-  }
-}
-
-class ProductCard extends StatelessWidget {
-  const ProductCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: kDefaultPadding,
-        vertical: kDefaultPadding / 2,
-      ),
-      // color: Colors.blueAccent,
-      height: 160,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          //THESE ARE THE BACKGROUND
-          Container(
-            height: 136,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(22),
-                color: kBlueColor,
-                boxShadow: [kDefaultShadow]),
-            child: Container(
-              margin: EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(22),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 160,
-              width: 200,
-              child: Image.asset(
-                "assets/images/Item_1.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: SizedBox(
-              height: 136,
-              width: size.width - 200,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    child: Text(
-                      "Classic Leather Arm Chair",
-                      style: Theme.of(context).textTheme.labelLarge,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: kDefaultPadding * 1.5, //30 padding
-                      vertical: kDefaultPadding / 4, //5 top and bottom
-                    ),
-                    decoration: BoxDecoration(
-                      color: kSecondaryColor,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(22),
-                        topRight: Radius.circular(22),
-                      ),
-                    ),
-                    child: Text(
-                      "\$58",
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
